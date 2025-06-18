6b59fc1176e2a8671823e6f61fe99d409b5dfe15
Heitor Coelho <heitor.santosprog@gmail.com>
2025-06-18 10:18:44 -0300

    feat: integração rest_framework_simplejwt urls

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
--- backend/smartinventory/urls.py
+++ backend/smartinventory/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.users.views import UserCreateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # refresh token
+
+
     path('api/inventory/', include('apps.inventory.urls')),
+    path('api/register/', UserCreateView.as_view(), name='user-register'),
 ]
